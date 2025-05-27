85ada724525adef495aa5f793698093f21ebe725
somethingwentwrongx <tymon.dydowicz@onet.pl>
2025-05-27 21:40:14 +0200

    Added condition for cooling

--- src/main/kotlin/QAP/SA/ICoolingSchedule.kt
+++ src/main/kotlin/QAP/SA/ICoolingSchedule.kt
@@ -3,19 +3,30 @@ package QAP.SA
 import kotlin.math.ln
 
 interface ICoolingSchedule {
+    fun shouldCool(iteration: Int, temperature: Double): Boolean
     fun cool(currentTemperature: Double): Double
 
-    class Exponential(private val alpha: Double = 0.95) : ICoolingSchedule {
+    class Exponential(private val coolingIterations: Int, private val alpha: Double = 0.95) : ICoolingSchedule {
+        override fun shouldCool(iteration: Int, temperature: Double): Boolean {
+            return iteration % coolingIterations == 0 && iteration > 0
+        }
+
         override fun cool(currentTemperature: Double): Double =
             currentTemperature * alpha
     }
 
-    class Linear(private val delta: Double = 1.0) : ICoolingSchedule {
+    class Linear(private val coolingIterations: Int, private val delta: Double = 1.0) : ICoolingSchedule {
+        override fun shouldCool(iteration: Int, temperature: Double): Boolean {
+            return iteration % coolingIterations == 0 && iteration > 0
+        }
         override fun cool(currentTemperature: Double): Double =
             (currentTemperature - delta).coerceAtLeast(0.0)
     }
 
-    class Logarithmic(private val gamma: Double = 1.0) : ICoolingSchedule {
+    class Logarithmic(private val coolingIterations: Int, private val gamma: Double = 1.0) : ICoolingSchedule {
+        override fun shouldCool(iteration: Int, temperature: Double): Boolean {
+            return iteration % coolingIterations == 0 && iteration > 0
+        }
         override fun cool(currentTemperature: Double): Double =
             currentTemperature / (1 + gamma * ln(1 + currentTemperature))
     }
